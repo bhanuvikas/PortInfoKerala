84d7dd67fe0bfd44b68fc15baab05cb37df4bb25
bhanuvikas <f20160106@hyderabad.bits-pilani.ac.in>
2018-07-08 23:26:26 +0530

    Just added the DialogFragmentDetails java file

---
+++ app/src/main/java/com/example/bhanu/portinfokerala/DialogFragmentDetails.java
@@ -0,0 +1,104 @@
+package com.example.bhanu.portinfokerala;
+
+public class DialogFragmentDetails {
+
+    int index;
+    String booking_id;
+    String port_name;
+    String zone_name;
+    String request_method;
+    String quantity;
+    String date;
+    String route;
+    String distance;
+    String request_status;
+    String current_status;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(String booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public String getPort_name() {
+        return port_name;
+    }
+
+    public void setPort_name(String port_name) {
+        this.port_name = port_name;
+    }
+
+    public String getZone_name() {
+        return zone_name;
+    }
+
+    public void setZone_name(String zone_name) {
+        this.zone_name = zone_name;
+    }
+
+    public String getRequest_method() {
+        return request_method;
+    }
+
+    public void setRequest_method(String request_method) {
+        this.request_method = request_method;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getRequest_status() {
+        return request_status;
+    }
+
+    public void setRequest_status(String request_status) {
+        this.request_status = request_status;
+    }
+
+    public String getCurrent_status() {
+        return current_status;
+    }
+
+    public void setCurrent_status(String current_status) {
+        this.current_status = current_status;
+    }
+}
